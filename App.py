4bc7d9d230ff3fd95851e04d8997892d7d032c8a
Sree Anondo Kumar Roy <spkroy2006@gmail.com>
2026-04-11 00:12:45 +0600

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template('index.html') # এইটাই আপনার হাব পেজ দেখাবে
+    return render_template('index.html') # এটি আপনার হোম পেজ দেখাবে
 
 @app.route('/chat')
 def chat():
-    return render_template('chat.html') # আপনার আগের চ্যাট ইন্টারফেস
+    return render_template('chat.html') # এটি চ্যাটবট পেজ দেখাবে
+
+if __name__ == "__main__":
+    app.run(debug=True)
